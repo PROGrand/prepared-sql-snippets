5da3eb2cf6b8a3333a1c02abfd30532aec9851c2
vladimir.koltunov <progrand@gmail.com>
2023-04-03 13:03:29 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "prepared_sql_snippets"
---
+++ src/main/kotlin/org/mtbo/preparedsqlsnippets/FlutterContext.kt
@@ -0,0 +1,11 @@
+package org.mtbo.preparedsqlsnippets
+
+import com.intellij.codeInsight.template.TemplateActionContext
+import com.intellij.codeInsight.template.TemplateContextType
+
+class FlutterContext private constructor() :
+    TemplateContextType( "PREPARED_SQL_SNIPPETS", "Prepared SQL snippets") {
+    override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
+        return templateActionContext.file.name.endsWith(".dart")
+    }
+}
